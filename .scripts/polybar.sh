eb40fe6a4ce0ebd07671e6e83008bf41b32095d6
Raphael Wuillemier <raphael@wuillemier.com>
2021-09-06 16:17:37 +0200

    chore: Removed obsolete files

--- .scripts/polybar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Kill running polybar
-killall -q polybar
-
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch polybar
-/home/raphael/.config/polybar/./launch.sh --docky
